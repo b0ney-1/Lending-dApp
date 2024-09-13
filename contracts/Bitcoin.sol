70477dca3831b16dace844b3612f6982930aa350
Boney Fernandez <boney.fernandez@metaschool.so>
2024-09-13 16:17:14 +0530

    Update Bitcoin.sol

--- contracts/Bitcoin.sol
+++ contracts/Bitcoin.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
-// 0xd9145CCE52D386f254917e481eB44e9943F39138
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract Bitcoin is ERC20, Ownable, ERC20Permit {
+contract USD is ERC20, Ownable, ERC20Permit {
     constructor(string memory name, string memory symbol)
         ERC20(name, symbol)
         Ownable(msg.sender)
@@ -15,7 +14,7 @@ contract Bitcoin is ERC20, Ownable, ERC20Permit {
     {}
 
     function mint(address to, uint256 amount) public {
-        require(amount <= 10 * 10 ** 18, "amount must be less than 10");
+        require(amount <= 100 * 10 ** 18, "amount must be less than 100");
         _mint(to, amount);
     }
 }
